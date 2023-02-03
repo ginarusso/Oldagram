dc755a0b4458ce2c202601ee8d0b386a007a85fb
Gina Russo <gpr0496@gmail.com>
2023-02-03 10:51:28 -0500

    upload new app

---
+++ index.js
@@ -0,0 +1,77 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        avatarDescr: "image of Vincent Van Gogh",
+        post: "images/post-vangogh.jpg",
+        postDescr: "image of Vincent VanGogh",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        avatarDescr: "image of Gustave Courbet",
+        post: "images/post-courbet.jpg",
+        postDescr: "image of Gustave Courbet",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        avatarDescr: "image of Joseph Ducreux",
+        post: "images/post-ducreux.jpg",
+        postDescr: "image of Joseph Ducreux",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const avatar = document.getElementById('avatar')
+const name = document.getElementById('name')
+const locationEl = document.getElementById('location')
+const portrait = document.getElementById('portrait')
+const heart = document.getElementById('heart-icon')
+const likes = document.getElementById('likes')
+const username = document.getElementById('username')
+const comment = document.getElementById('comment')
+
+const randomNum = Math.floor(Math.random() * 3)
+
+function getRandomPost() {
+    avatar.src = `${posts[randomNum].avatar}`
+    name.textContent = `${posts[randomNum].name}`
+    locationEl.textContent = `${posts[randomNum].location}`
+    portrait.src = `${posts[randomNum].post}`
+    likes.textContent = `${posts[randomNum].likes} likes`
+    username.textContent = `${posts[randomNum].username}`
+    comment.textContent = `${posts[randomNum].comment}`
+}
+
+getRandomPost()
+
+// const allPosts = posts.map(post => {
+//     // console.log(post.avatar)
+// avatar.src = `${post.avatar}`
+// name.textContent = `${post.name}`
+// locationEl.textContent = `${post.location}`
+// portrait.src = `${post.post}`
+// likes.textContent = `${post.likes} likes`
+// username.textContent = `${post.username}`
+// comment.textContent = `${post.comment}`
+// })
+
+// heart.addEventListener("clicked", function likePost() {
+//    console.log("clicked") 
+// })
+// avatar.innerHTML = `${post[0].avatar}`
+// avatarEl.src = `images/avatar-vangogh.jpg`
+
+// console.log(posts[0].avatar)
